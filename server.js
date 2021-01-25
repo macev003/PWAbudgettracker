50e33808fc55f3266f862f9bcb108603ad6b7cdf
Maira Acevedo <m.acevedo53@yahoo.com>
2021-01-24 22:07:01 -0800

    updated routes in server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const apiRoutes = require("./routes/api.js");
 
 const PORT = 3000;
 
@@ -24,7 +25,7 @@ mongoose.connect(
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
